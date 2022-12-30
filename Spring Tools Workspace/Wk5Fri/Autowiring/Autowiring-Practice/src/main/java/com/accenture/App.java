2c3158aa504bc645902c8acda463bbe439e4e499
tangjeraldine <typjera.1127@gmail.com>
2022-12-30 18:44:25 +0800

    First commit.

---
+++ Spring Tools Workspace/Wk5Fri/Autowiring/Autowiring-Practice/src/main/java/com/accenture/App.java
@@ -0,0 +1,20 @@
+package com.accenture;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new AnnotationConfigApplicationContext(Car.class);
+    	
+    	// use context to obtain the bean that you declared earlier --> carbean
+    	Car car = (Car) context.getBean("carbean");
+    	//Tyre constructor gets printed first because it's a singleton and got instantiated the moment context was created
+    	
+    	//Followed by this:
+    	System.out.println("This is a car object: "+car);
+    	car.info();
+    }
+}
---
+++ Spring Tools Workspace/Wk5Fri/Autowiring/Autowiring-Practice/src/main/java/com/accenture/two/Tyre.java
@@ -0,0 +1,10 @@
+package com.accenture.two;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tyre {
+	public Tyre() {
+		System.out.println("I'm a tyre! I belong on a car!");
+	}
+}
